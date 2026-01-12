d7b949d16d4fab40fc6c1e42333f4919b7b8a2af
Deepak Sharma <I42928@woodmacad.com>
2026-01-12 20:01:13 +0530

    Add topic creation scaffolding and persistence services
    Added placeholder classes for topic creation (command, handler, validator) in Application layer. Updated project files to include new folders. Introduced ServiceExtension for Application and Persistence layers, with persistence configuration for DbContext and audit interceptor. Replaced Scripts folder with Exceptions in Domain project.

---
+++ InterviewCoach.Infrastructure.Persistence/ServiceExtension.cs
@@ -0,0 +1,31 @@
+﻿using InterviewCoach.Infrastructure.Persistence.Database.Entities;
+using InterviewCoach.Infrastructure.Persistence.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InterviewCoach.Infrastructure.Persistence
+{
+    public static class ServiceExtension
+    {
+        public static void ConfigurePersistenceService(this IServiceCollection services, string DbConnectionString)
+        {
+            services.AddDbContext<ApplicationContext>((builder, options) =>
+            {
+                options.AddInterceptors(builder.GetService<ISaveChangesInterceptor>()!);
+                options.UseSqlServer(DbConnectionString, builder =>
+                {
+                    builder.EnableRetryOnFailure(
+                           maxRetryCount: 5,
+                           maxRetryDelay: TimeSpan.FromSeconds(30),
+                           errorNumbersToAdd: null);
+
+                    builder.CommandTimeout(60);
+                }).EnableDetailedErrors()
+                  .EnableSensitiveDataLogging();
+            });
+
+            services.AddScoped<ISaveChangesInterceptor, AuditEntityInterceptors>();
+        }
+    }
+}
