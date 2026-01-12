4db55b0f968bf0d0c0dc92101fca0ad74494fe1a
Deepak Sharma <I42928@woodmacad.com>
2026-01-12 17:12:34 +0530

    Introduce CQRS base with MediatR and project references
    Added CQRS abstractions (ICommand, IQuery, handlers) in Application layer and set up MediatR. Updated project references to support clean architecture. Added BaseController in API for MediatR integration.

---
+++ InterviewCoach.Application/Abstractions/IQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCoach.Application.Abstractions
+{
+    public interface IQueryHandler<TQuery, TRequest> : IRequestHandler<TQuery, TRequest> where TQuery : IQuery<TRequest> where TRequest : notnull
+    {
+    }  
+}
