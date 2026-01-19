f69b64f67dd2b4b4589d3962ba434a3c791a8a5c
Deepak Sharma <I42928@woodmacad.com>
2026-01-19 23:21:30 +0530

    Add initial TopicController as API endpoint scaffold
    Introduced a new TopicController inheriting from BaseController, decorated with [ApiController] and [Route("api/[controller]")]. This sets up the basic API endpoint for topics, though no action methods have been implemented yet. Includes necessary using directives for ASP.NET Core MVC and HTTP.

---
+++ InterviewCoach.API/Controllers/TopicController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewCoach.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopicController : BaseController
+    {
+    }
+}
