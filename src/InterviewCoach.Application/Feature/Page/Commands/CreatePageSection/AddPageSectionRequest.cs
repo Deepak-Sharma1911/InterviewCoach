7b669623e777dc80cb60dbaf0360c328a3a7dde0
Deepak Sharma <I42928@woodmacad.com>
2026-02-21 19:27:41 +0530

    Code Changes for running API

---
+++ src/InterviewCoach.Application/Feature/Page/Commands/CreatePageSection/AddPageSectionRequest.cs
@@ -0,0 +1,6 @@
+﻿using InterviewCoach.Domain.Entities;
+
+namespace InterviewCoach.Application.Feature.Page.Commands.CreatePageSection
+{
+    public record AddPageSectionRequest(PageSectionType Type, string Title, string Content, int DisplayOrder);
+}
